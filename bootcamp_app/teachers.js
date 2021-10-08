4c64dfb9c94daf330b07ed5e2310b1099678d551
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-10-08 18:18:59 +0000

    adding parameterized qyeries to teachers and students js files

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -16,16 +16,23 @@ client.connect()
 
 
 
-const cohortName = process.argv[2]
-client.query(`
+
+const cohortName = process.argv[2] || 'JUL02';
+
+const values = [cohortName];
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort 
 FROM teachers 
 JOIN assistance_requests ON teacher_id = teachers.id 
 JOIN students ON student_id = students.id 
 JOIN cohorts ON cohort_id = cohorts.id 
-WHERE cohorts.name = '${cohortName || 'JUL02'}'
-ORDER BY teacher;
-`)
+WHERE cohorts.name = $1
+ORDER BY teacher
+`;
+
+
+client.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort} : ${user.teacher}`);
